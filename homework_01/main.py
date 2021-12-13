ce2a61a0ccb5e7ff398a5b66407c7e3edda154f9
“Vasendin” <a.vasendin@mail.ru>
2021-12-13 23:52:56 +0300

    power_numbers and filter_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,22 +4,50 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [n**2 for n in numbers]
+
+
+def is_prime(n):
+    den = 2
+    if n > 1:
+        while n % den != 0:
+            den = den + 1
+        if den == n:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def is_odd(n):
+    if n % 2 == 0:
+        return False
+    else:
+        return True
+
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
 
 
 # filter types
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
+ODD = is_odd
+EVEN = is_even
+PRIME = is_prime
 
 
-def filter_numbers():
+def filter_numbers(*numbers, ft):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +58,4 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    return list(filter(ft, numbers))
