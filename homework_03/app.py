45342c38087e52b8e812e94a0938cd2c61d6afbc
“Vasendin” <a.vasendin@mail.ru>
2022-02-05 22:01:16 +0300

    hw_03 small_correction

--- homework_03/app.py
+++ homework_03/app.py
@@ -1,23 +1,14 @@
-from typing import Optional
-import uvicorn
-import os
-from fastapi import FastAPI, Body, HTTPException, status, Depends, Query
+from fastapi import FastAPI
 
 
 app = FastAPI(title="Ping pong")
 print("RUN")
 
 @app.get("/")
-def read_root(
-    q: Optional[str] = Query(None),
-):
+def read_root():
     return "Hello"
 
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
-
-
-# if __name__ == '__main__':
-#     uvicorn.run(app, host="0.0.0.0", port=8080)
